dbeb0b4563d54993ac0c8fd13350c7fa70f72f17
Mohamed Salah <midosalah25552@gmail.com>
2026-06-14 17:38:20 +0300

    pp

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,6 +1,7 @@
 import { clearToken, getToken } from "@/lib/auth";
 
 const BASE = import.meta.env.VITE_API_URL || "http://localhost:4000";
+//* karim has no pp 
 
 export class ApiError extends Error {
   status: number;
